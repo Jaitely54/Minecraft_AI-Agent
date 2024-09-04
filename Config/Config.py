a9a64a0beb3f2b916148e4ae39f4f1719064e06b
Kritish Jaitely <136069402+Jaitely54@users.noreply.github.com>
2024-09-04 21:28:57 +0530

    Create Config.py

---
+++ Config/Config.py
@@ -0,0 +1,5 @@
+class Config:
+    HOST = 'ChuranPlayground.aternos.me'  # Replace with your server IP/hostname
+    PORT = 32331                          # Replace with your server port
+    USERNAME = 'lee-mon'                   # Bot's in-game name
+
